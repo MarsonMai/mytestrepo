717c739fe218441a07bd2d45cf08a1c7b1bc8363
BT_1N3_22 <a@gmail.com>
2023-12-15 09:04:06 -0500

    added some print statements

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,10 @@
 public class Main {
     public static void main(String[] args) {
+        Test x = new Test();
         System.out.println("Hello world!");
         System.out.println("Mmm");
         System.out.println(" awawdjfjfafafw");
+        x.Class(20);
     }
 
 }
